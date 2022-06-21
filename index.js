b019e75ba016e9c9d4714cbc6c9d09f2bbb26626
Rodolpho Almeida <rodolpho_almeida@hotmail.com>
2022-06-21 13:19:28 -0300

    ajustes cookie

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const userRoutes = require('./routes/userRoutes')
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
-app.use(cors({origin: false, origin: '*', methods: 'GET,PUT,POST', credentials: false}))
+app.use(cors({origin: false, origin: 'https://relatorio-de-horas.herokuapp.com/superuser/dashboard/*', methods: 'GET,PUT,POST', credentials: true}))
 
 app.use('/superUsers', superUserRoutes)
 app.use('/congregation', congregationsRoutes)
