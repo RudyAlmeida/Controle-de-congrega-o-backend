631819de9e3f32ad2a46b74c1772722cbd42db8e
Rodolpho Almeida <rodolpho_almeida@hotmail.com>
2022-06-21 14:19:35 -0300

    ajustes cookie

--- helpers/superUserHelper.js
+++ helpers/superUserHelper.js
@@ -4,13 +4,13 @@ const jwtSecret = process.env.JWT_SECRET;
 
 const superUserAuth = (req, res, next) => {
   const usertoken = req.body.token;
-  console.log(req.body)
+  console.log(req.body.token)
   delete req.body.token
   if (usertoken) {
     // const token = usertoken.split(" ");
-    const newToken = usertoken.split("=");
-    const jwtToken = newToken[1].split(";");
-    jwt.verify(jwtToken[0], jwtSecret, (err, decodedToken) => {
+    //const newToken = usertoken.split("=");
+    //const jwtToken = newToken[1].split(";");
+    jwt.verify(usertoken[0], jwtSecret, (err, decodedToken) => {
       if (err) {
         return res.status(401).json({ message: "Cookie not valid" });
       } else {
