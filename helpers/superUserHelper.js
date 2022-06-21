74ed6185f61af9027e9bc1e5e79fdc0cef75e42a
Rodolpho Almeida <rodolpho_almeida@hotmail.com>
2022-06-21 01:36:27 -0300

    conferindo cookie

--- helpers/superUserHelper.js
+++ helpers/superUserHelper.js
@@ -5,10 +5,13 @@ const jwtSecret = process.env.JWT_SECRET;
 const superUserAuth = (req, res, next) => {
   const usertoken = req.headers.cookie;
   if (usertoken) {
+    console.log('Recebido ' +token)
     const token = usertoken.split(" ");
     const newToken = token[1].split("=");
     const jwtToken = newToken[1].split(";");
+    console.log('Ultimo split '+jwtToken)
     jwt.verify(jwtToken[0], jwtSecret, (err, decodedToken) => {
+      console.log('decoded '+decodedToken)
       if (err) {
         return res.status(401).json({ message: "Not authorized" });
       } else {
